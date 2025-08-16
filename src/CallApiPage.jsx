9e39284de6dbec0a80cac9618b9db155d2dd80bf
kien nguyen <kienhd2004@gmail.com>
2025-08-16 14:20:23 +0700

    update

--- src/CallApiPage.jsx
+++ src/CallApiPage.jsx
@@ -1,50 +1,41 @@
 import React from "react";
 import dayjs from "dayjs";
-import { createLeaveApplication } from "./services/leaveApplication";
-
+import { formInstance } from "./formInstance";
 export default function TestPost() {
   const handleSubmit = async () => {
     try {
-      const file = new File(["Test file"], "test.txt", { type: "text/plain" });
+      const formData = new FormData();
 
-      const data = {
-        maximum_leave_day: 12,
-        leave_day_taken: 3,
-        remaining_leave_day: 9,
-        application_date: dayjs(),
-        start_date: dayjs("2025-08-20"),
-        end_date: dayjs("2025-08-22"),
-        reason: "Nghỉ phép cá nhân",
-        approver_id: 1,
-        leave_day: 2,
-        substitute_person_id: 2,
-        leave_type_id: 1,
-        related_person_id: 3,
-        proposed_status_id: 1,
-        paid_rate_id: 1,
-        note: "Ghi chú",
-        attached_file: file,
-      };
+      formData.append("maximum_leave_day", 12);
+      formData.append("leave_day_taken", 3);
+      formData.append("remaining_leave_day", 9);
+      formData.append("application_date", dayjs().format("YYYY-MM-DD"));
+      formData.append("start_date", dayjs("2025-08-20").format("YYYY-MM-DD"));
+      formData.append("end_date", dayjs("2025-08-22").format("YYYY-MM-DD"));
+      formData.append("reason", "Nghỉ phép cá nhân");
+      formData.append("approver_id", 1);
+      formData.append("leave_day", 2);
+      formData.append("substitute_person_id", 2);
+      formData.append("leave_type_id", 1);
+      formData.append("related_person_id", 3);
+      formData.append("proposed_status_id", 1);
+      formData.append("paid_rate_id", 1);
+      formData.append("paid_rate", 1);
+      formData.append("note", "Ghi chú");
+      formData.append(
+        "attached_file",
+        new File(["Test file"], "test.txt", { type: "text/plain" })
+      );
 
-      const result = await createLeaveApplication(data);
+      const result = await formInstance.post(
+        "/hrm_leave_application",
+        formData
+      );
       console.log("Kết quả:", result);
     } catch (error) {
       console.error("Lỗi:", error);
     }
   };
 
-  const callApi = async () => {
-    setLoading(true);
-    try {
-      // Ví dụ GET: /test
-      const res = await formInstance.get("/test");
-      setResult(res.data);
-    } catch (err) {
-      console.error(err);
-      setResult({ error: err.message });
-    } finally {
-      setLoading(false);
-    }
-  };
   return <button onClick={handleSubmit}>Gửi đơn nghỉ phép</button>;
 }
